cbcb39febc7ab5fd231f080560b62322fee2d0bb
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2026-04-21 10:31:24 +0700

    feat: implement UserController with CRUD operations for users

--- src/main/java/com/deployment/praktikum8/controllers/UserController.java
+++ src/main/java/com/deployment/praktikum8/controllers/UserController.java
@@ -1,5 +1,47 @@
 package com.deployment.praktikum8.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+import com.deployment.praktikum8.models.User;
+import com.deployment.praktikum8.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
-    
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request){
+        userService.addUser(request);
+        return "user berhasil di tambahkan";
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
+        return "user berhasil di hapus";
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id){
+        return userService.getUserById(id);
+    }
 }
