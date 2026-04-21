fb3add3cb2de87d1be6208cdb849e281e6881b1d
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2026-04-21 10:33:52 +0700

    feat: add update user functionality in UserController

--- src/main/java/com/deployment/praktikum8/controllers/UserController.java
+++ src/main/java/com/deployment/praktikum8/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +44,11 @@ public class UserController {
     @GetMapping("/{id}")
     public User getUserById(@PathVariable String id){
         return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User request){
+        userService.updateUser(id, request);
+        return "user berhasil di update";
     }
 }
