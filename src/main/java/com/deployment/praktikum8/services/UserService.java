30a905d579c4b0787c0671e305b463782f2b6185
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2026-04-21 10:12:51 +0700

    feat: update on UserService add feature for get all data User from UserModel

--- src/main/java/com/deployment/praktikum8/services/UserService.java
+++ src/main/java/com/deployment/praktikum8/services/UserService.java
@@ -1,6 +1,10 @@
 package com.deployment.praktikum8.services;
 
 import com.deployment.praktikum8.models.User;
+
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.deployment.praktikum8.repository.UserRepository;
@@ -13,7 +17,14 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User addUser(User user) {
-        return userRepository.save(user);
+    public User addUser(User request) {
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
     }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    
 }
