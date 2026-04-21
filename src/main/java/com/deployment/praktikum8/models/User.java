e990f31496cdc40546d3192780d77eff4f662b78
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2026-04-21 09:53:30 +0700

    feat: update user models

--- src/main/java/com/deployment/praktikum8/models/User.java
+++ src/main/java/com/deployment/praktikum8/models/User.java
@@ -1,19 +1,28 @@
 package com.deployment.praktikum8.models;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@Table(name = "20240140037_users")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class User {
-    
+    @Id
     private String id;
 
     @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private String nim;
 }
